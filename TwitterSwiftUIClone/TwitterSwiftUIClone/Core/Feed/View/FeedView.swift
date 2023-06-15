8f7e0409ba33d9d8de070c38781af8b4133ad219
syunaev <safiya.yunaeva@gmail.com>
2023-06-14 18:03:09 -0700

    Starting SwiftUIClone project

---
+++ TwitterSwiftUIClone/TwitterSwiftUIClone/Core/Feed/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Sonya on 6/14/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
---
+++ TwitterSwiftUIClone/TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Sonya on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
